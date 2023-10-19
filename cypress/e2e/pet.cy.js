0175ee77dc04a2eee92d8c726a1ddd1060cfcb38
Leonardo Badell <leonardo.badell@pris.com.br>
2023-10-18 22:39:40 -0300

    Adding changes of cy.resquest methods without Custom command

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -41,3 +41,50 @@ describe('Testing the Swagger Petstore', () => {
         });
       });
     });
+
+
+//This cy.request doesn't have Custom Command on the support/commands.js
+describe('Updates a pet in the store with form data', () => {
+  it('Should update a pet in the store', () => {
+    
+    const bodyRequest = {
+      "name": "Canin",
+      "status": "pending"
+      }
+      
+      cy.request({
+        method: 'POST',
+        url: 'https://petstore.swagger.io/v2/pet/532321',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: bodyRequest,
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('code', 200);
+        expect(response.body).to.have.property('type', 'unknown');
+        expect(response.body).to.have.property('message', '532321');
+      })
+    })
+  });  
+
+  
+
+//This cy.request doesn't have Custom Command on the support/commands.js
+describe('Deletes a pet', () => {
+  it.only('Should delete a pet', () => {
+    cy.request({
+      method: 'DELETE',
+      url: 'https://petstore.swagger.io/v2/pet/696461602',
+      headers: {
+          'api-key': 'special-key',
+          'Content-Type': 'application/json'
+        },
+      followRedirect: true,
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+      });
+    });
+  });
+
+  
